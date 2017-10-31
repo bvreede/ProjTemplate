47b1378c8c99baa15ca30280f8c0c8b89133bcdf
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-10-30 22:31:52 -0400

    Changed installed.packages to installed.packages() in add_versions

--- R/add_versions.R
+++ R/add_versions.R
@@ -3,7 +3,7 @@
 #' @return
 add_versions <- function(pkgs) {
   out <- dplyr::select(
-    dplyr::filter(as.data.frame(installed.packages), Package %in% pkgs),
+    dplyr::filter(as.data.frame(installed.packages()), Package %in% pkgs),
     Package, Version)
   out <- paste(out$Package, out$Version, collapse='=>')
   return(out)
