ce3716aa80dd2a2a4abe3faa97d8a55867fad097
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2017-12-12 15:43:53 -0500

    Updated Github load, resolves #3

--- R/load_packages.R
+++ R/load_packages.R
@@ -11,27 +11,32 @@
 #'
 #' @examples
 load_packages <- function(homedir = '.') {
+  `%notin%` <- Negate('%in%')
   if(!file.exists(file.path('lib','pkgs.yml'))){
     add_package('ProjTemplate')
   }
   pkgs <- yaml::yaml.load_file(file.path('lib','pkgs.yml'))
-  if(any(stringr::str_detect(pkgs, '=>'))){# Version information included
+  if(all(stringr::str_detect(pkgs, '=>'))){# Version information included
     out = do.call(rbind,stringr::str_split(pkgs,'=>'))
     pkgs <- out[,1]
     versions <- out[,2]
   } else {versions <- NULL}
   for(p in pkgs){
-    if(!(p %in% installed.packages()[,1])){
-      if(!is.null(versions)){
-        devtools::install_version(p, version = out[out[,1]==p,2], repos = 'http://cran.rstudio.com')
-      } else{
-        if (str_detect(p, '/')){
-          devtools::install_github(p)
+    if (str_detect(p, '/')){ # GitHub packages
+      pkgname = stringr::str_split(p, '/')[[1]][[2]]
+      if (pkgname %notin% installed.packages()[,1]){
+        devtools::install_github(p)
+      }
+      suppressPackageStartupMessages(library(p, character.only = TRUE))
+    } else { # CRAN packages
+      if(!(p %in% installed.packages()[,1])){
+        if(!is.null(versions)){
+          devtools::install_version(p, version = out[out[,1]==p,2], repos = 'http://cran.rstudio.com')
         } else{
-        utils::install.packages(p, repos='http://cran.rstudio.com')
+          utils::install.packages(p, repos='http://cran.rstudio.com')
         }
       }
+      suppressPackageStartupMessages(library(p, character.only = TRUE))
     }
-    suppressPackageStartupMessages(library(p, character.only = TRUE))
   }
 }
