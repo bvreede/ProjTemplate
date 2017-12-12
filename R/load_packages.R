802f024954cc91ca9936907ce556c3871f7f6cf9
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2017-12-12 15:33:20 -0500

    Updated load_packages to try GitHub

--- R/load_packages.R
+++ R/load_packages.R
@@ -3,6 +3,9 @@
 #' This function reads a file "pkgs.yml" in the `lib` directory of the project
 #' and loads them in R, installing them if necessary
 #'
+#' Non-CRAN packages can be accessed via GitHub by specifying `user/package` format
+#' Particular versions of packages can be ensured by specifying `package => version` format
+#'
 #' @return
 #' @export
 #'
@@ -12,17 +15,21 @@ load_packages <- function() {
     stop('No packages to load; use `add_package` to add packages')
   }
   pkgs <- yaml::yaml.load_file(file.path('lib','pkgs.yml'))
-  if(all(stringr::str_detect(pkgs, '=>'))){# Version information included
+  if(any(stringr::str_detect(pkgs, '=>'))){# Version information included
     out = do.call(rbind,stringr::str_split(pkgs,'=>'))
     pkgs <- out[,1]
-    versions <- str[,2]
+    versions <- out[,2]
   } else {versions <- NULL}
   for(p in pkgs){
     if(!(p %in% installed.packages()[,1])){
       if(!is.null(versions)){
         devtools::install_version(p, version = out[out[,1]==p,2], repos = 'http://cran.rstudio.com')
       } else{
+        if (str_detect(p, '/')){
+          devtools::install_github(p)
+        } else{
         utils::install.packages(p, repos='http://cran.rstudio.com')
+        }
       }
       suppressPackageStartupMessages(library(p, character.only = TRUE))
     }
